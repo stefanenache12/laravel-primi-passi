b0e41d45337c2bb9fde59375086aec87649ba7cc
stefanenache12 <stefanenache12@gmail.com>
2023-09-07 17:40:02 +0200

    first commit

---
+++ resources/views/components/header-component.blade.php
@@ -0,0 +1,18 @@
+<header>
+    <nav>
+        <div class="logo">
+            <a href="/">
+                <h1>
+                    My First Laravel Project
+                </h1>
+            </a> 
+        </div>
+        <div>
+            <ul>
+                <li><a href="/">HOME</a></li>
+                <li><a href="/about-us">ABOUT US</a></li>
+                <li><a href="/contacts">CONTACTS</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
